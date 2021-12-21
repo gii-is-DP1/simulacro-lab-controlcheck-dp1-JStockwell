b351dd91135b3d8f7f98c8dbaf0f32809d7c26d0
Jaime Stockwell Mendoza <jstockwellmendoza@gmail.com>
2021-12-21 11:17:28 +0100

    Ex1 Incomplete

--- src/main/java/org/springframework/samples/petclinic/product/Product.java
+++ src/main/java/org/springframework/samples/petclinic/product/Product.java
@@ -1,13 +1,34 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @PrimaryKeyJoinColumn
     Integer id;
+
+    @NotNull
+    @Size(min = 3, max = 50)
     String name;
+
+    @Min(0)
+    @NotNull
     double price;
+
+    @ManyToOne
     ProductType productType;
 }
