7232e092bef7483fb6cb7bc7bdd8b0efdf0698e5
Jaime Stockwell Mendoza <jstockwellmendoza@gmail.com>
2021-12-21 12:28:22 +0100

    final commit
    menos mal que esto no es el examen

--- src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
@@ -3,12 +3,17 @@ package org.springframework.samples.petclinic.product;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 
 
-public interface ProductRepository {
+public interface ProductRepository extends CrudRepository<Product, String>{
     List<Product> findAll();
+    @Query(
+        value = "SELECT * FROM product_types", 
+        nativeQuery = true
+    )
     List<ProductType> findAllProductTypes();
     Optional<Product> findById(int id);
     Product findByName(String name);
