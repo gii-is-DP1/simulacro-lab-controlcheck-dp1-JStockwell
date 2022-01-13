a4c07bd3c55639b18314532e5c9f9cd1535ab9d7
Stockie <jstockwellmendoza@gmail.com>
2022-01-13 18:58:10 +0100

    fix: @table to product types

--- src/main/java/org/springframework/samples/petclinic/product/ProductType.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductType.java
@@ -1,6 +1,7 @@
 package org.springframework.samples.petclinic.product;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 import org.springframework.samples.petclinic.model.NamedEntity;
 
@@ -10,6 +11,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
+@Table(name = "product_types")
 public class ProductType extends NamedEntity {
 
 }
