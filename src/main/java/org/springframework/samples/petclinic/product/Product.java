fd03debfe976ee0ce2a3916cbacc5fccc78d5bc7
Stockie <jstockwellmendoza@gmail.com>
2022-01-13 18:32:22 +0100

    feat: ejercicio 1

--- src/main/java/org/springframework/samples/petclinic/product/Product.java
+++ src/main/java/org/springframework/samples/petclinic/product/Product.java
@@ -1,13 +1,16 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.persistence.Entity;
+
+import org.springframework.samples.petclinic.model.NamedEntity;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Product {
-    Integer id;
-    String name;
+@Entity
+public class Product extends NamedEntity {
     double price;
     ProductType productType;
 }
